c7b235d4e8de7052d174597fec876bc7725fbdbb
Jonathan <jalcantara1990@gmail.com>
2017-11-22 00:41:32 +0100

    question detail finished

--- src/app/answer/answer-form.component.ts
+++ src/app/answer/answer-form.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Answer, User } from './answer.model';
+import { Question } from '../question/question.model';
+
 @Component({
   selector: 'app-answer-form',
-  templateUrl: './answer-form.component.html'
+  templateUrl: './answer-form.component.html',
+  styles: [
+    `
+    form {
+      display: flex;
+      flex-direction: column;
+      width: 90%;
+      margin: 40px auto;
+      margin-top: 20px;
+    } 
+  `]
 })
 export class AnswerFormComponent  {
+  @Input() question: Question;
 
   onSubmit(form: NgForm) {
-    console.log(form.value.description);
+    const answer = new Answer(
+      form.value.description,
+      this.question,
+      new Date(),
+      new User('Jonathan', 'Alcántara')
+    );
+
+    this.question.answers.unshift(answer);
+    form.reset();
   }
 
 }
